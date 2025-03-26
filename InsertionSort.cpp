34404b279d527ca91cc170a1bdc82bfe888b2b66
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-26 23:07:20 +0700

    Membuat jarak baris, menggunakan perulangan dan memasukkan input nilai

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,3 +20,13 @@ void input()
             cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika lebih dari 20
         }
     }
+    cout << endl; // Membuat jarak per baris program
+    cout << "======================" << endl; // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i= 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; //Memasukkan atau menginputkan nilai data n
+    }
+}
